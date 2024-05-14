7cf3250b9646fbfcb830826f7e2bdb40c295d861
dmawardi <dmawardi@gmail.com>
2024-05-14 13:59:01 +0800

    Added order model

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Screen\AsSource;
+
+class Order extends Model
+{
+    use HasFactory, AsSource;
+
+    public $guarded = [];
+
+    public function items()
+    {
+        // return $this->hasMany(OrderItem::class);
+    }
+}

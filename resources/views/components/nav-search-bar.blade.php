f29d143adf8c492a18f199783982929f22925b5e
dmawardi <dmawardi@gmail.com>
2024-05-23 14:56:58 +0800

    Completed new navbar

---
+++ resources/views/components/nav-search-bar.blade.php
@@ -0,0 +1,7 @@
+<div class="hidden sm:block my-auto">
+    <form class="form-inline flex flex-row">
+        <input class="form-control mr-sm-2 rounded-md h-4/6 my-auto" type="search" placeholder="Search"
+            aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0 mx-2 bg-black text-white p-2" type="submit">Search</button>
+    </form>
+</div>

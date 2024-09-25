3506b4d174472f4f210cf9b8ab0fc4c71a80a079
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-09-25 08:59:47 +0800

    Moved animations code for main carousel to animations.js

--- resources/js/animations.js
+++ resources/js/animations.js
@@ -1,3 +1,69 @@
+// Main Carousel
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.getElementById("main-carousel");
+    const prevButton = document.getElementById("main-carousel-prev");
+    const nextButton = document.getElementById("main-carousel-next");
+    const items = carousel.children;
+    let itemWidth = items[0].offsetWidth;
+    let visibleItems = Math.floor(
+        carousel.parentElement.offsetWidth / itemWidth
+    );
+    let index = 0;
+
+    // Update button visibility initially
+    updateButtonVisibility();
+
+    prevButton.addEventListener("click", function () {
+        if (index > 0) {
+            index--;
+            updateCarousel();
+        }
+    });
+
+    nextButton.addEventListener("click", function () {
+        if (index < items.length - visibleItems) {
+            index++;
+            updateCarousel();
+        }
+    });
+
+    function updateCarousel() {
+        console.log("Updating carousel");
+        carousel.style.transform = `translateX(-${index * itemWidth}px)`;
+        updateButtonVisibility();
+    }
+
+    function updateButtonVisibility() {
+        // Hide prev button if at the first item
+        if (index === 0) {
+            prevButton.classList.add("hidden");
+            prevButton.disabled = true;
+        } else {
+            prevButton.classList.remove("hidden");
+            prevButton.disabled = false;
+        }
+
+        // Hide next button if last item is fully visible
+        if (index >= items.length - visibleItems) {
+            nextButton.classList.add("hidden");
+            nextButton.disabled = true;
+        } else {
+            nextButton.classList.remove("hidden");
+            nextButton.disabled = false;
+        }
+    }
+
+    // Recalculate the number of visible items and update the carousel on window resize
+    window.addEventListener("resize", function () {
+        // Recalculate item width and visible items
+        itemWidth = items[0].offsetWidth;
+        visibleItems = Math.floor(
+            carousel.parentElement.offsetWidth / itemWidth
+        );
+        updateCarousel();
+    });
+});
+
 // Product Collection Row Carousel
 document.addEventListener("DOMContentLoaded", function () {
     // Select all carousels on the page

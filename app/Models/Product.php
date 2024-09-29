9b4fbd81730daf7844196f80a02089cc8bc2af3c
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-09-29 11:27:42 +0800

    Added Brand model and added relationship to Product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,4 +21,9 @@ class Product extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }

6e5d39e5574d489f7d4c3fd9ac8a22a87ef2f4f2
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-10-02 11:03:53 +0800

    Cleaned brand index page

--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -1,23 +1,30 @@
 <x-app-layout>
     <div class="mt-5 w-2/3 mx-auto">
-        <h1>Brands A-Z</h1>
+        <h1 class="text-2xl font-bold">Brands A-Z</h1>
 
-        <h2>
+        <!-- Separator -->
+        <div class="my-4 border-b-2 border-gray-300"></div>
+        <h2 class="font-bold text-xl">
             All Brands
         </h2>
 
+        <!-- Separator -->
+        <div class="my-4 border-b-2 border-gray-300"></div>
         @php
         $alphabets = range('A', 'Z');
         @endphp
-        @foreach ($alphabets as $alphabet)
-            <a href="#brand-{{$alphabet}}">{{$alphabet}}</a>
-        @endforeach
+        <div>
+            @foreach ($alphabets as $alphabet)
+                <a href="#brand-{{$alphabet}}" class="font-bold text-lg md:text-xl mx-2">{{$alphabet}}</a>
+            @endforeach
+        </div>
+
+        <!-- Separator -->
+        <div class="my-4 border-b-2 border-gray-300"></div>
+
 
         @foreach ($alphabets as $alphabet)
-            <div class="my-8">
-                <!-- Separator -->
-                <div class="my-4 border-b-2 border-gray-300"></div>
-
+            <div class="py-2">
                 <!-- Alphabet Heading -->
                 <h2 id="brand-{{ $alphabet }}" class="text-xl font-semibold">{{ $alphabet }}</h2>
 
@@ -27,7 +34,7 @@
                     <div class="columns-1 sm:columns-2 lg:columns-4">
                         <ul class="list-none">
                             @foreach ($groupedBrands[$alphabet] as $brand)
-                                <li class="my-1">
+                                <li>
                                     <a href="{{ route('brands.show', $brand->slug) }}" class="hover:underline">
                                         {{ $brand->name }}
                                     </a>
@@ -38,7 +45,10 @@
                 @else
                     <p>No brands starting with {{ $alphabet }}.</p>
                 @endif
+                <!-- Separator -->
+                <div class="mb-3 mt-8 border-b-2 border-gray-300"></div>
             </div>
         @endforeach
+        
     </div>
 </x-app-layout>

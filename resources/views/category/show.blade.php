e08898266c8a80fc934a76e222f348e7a19aacd9
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-10-03 17:15:22 +0800

    Category page meta tags added

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+<x-app-layout title="Shop {{ $category->name }} Products | Mona" 
+description="Explore the best in {{ $category }} at Mona. From foundations to lipsticks, find top-rated products that suit every skin tone and style." 
+keywords="{{ $category->name }}, makeup, makeup products, beauty, premium cosmetics, makeup shopping, Mona, beauty store" canonical="{{ route('categories.show', $category->slug) }}">
     <x-layouts.breadcrumbs :category="$category" :ancestors="$ancestors"></x-layouts.breadcrumbs>
     <div class="flex flex-wrap md:flex-nowrap">
         <x-search.filters :label="$category->name" :brands="$brands" routeAction="{{ route('categories.show', $category->slug) }}"></x-search.filters>

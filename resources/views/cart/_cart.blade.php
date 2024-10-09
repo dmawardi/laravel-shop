022f815ef5cb661fe3ed78976cff74339945318d
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-10-09 11:11:53 +0800

    Updated _cart include to have entire cart table

--- resources/views/cart/_cart.blade.php
+++ resources/views/cart/_cart.blade.php
@@ -1,22 +1,29 @@
-<div>
-    <h3 class="font-medium">Your Cart Items</h3>
-    @if (session('cart'))
-    <ul>
-            @php $overallTotal = 0; @endphp
-            @foreach (session('cart') as $id => $details)
-                <!-- Add total -->
-                @php $overallTotal += $details['subtotal']; @endphp
-
-                <li class="mb-4">
-                    {{ $details['name'] }} - ${{ number_format($details['price'], 2) }} x
-                    {{ $details['quantity'] }}
-                    <span class="font-bold">Total:
-                        ${{ number_format($details['price'] * $details['quantity'], 2) }}</span>
-                </li>
-            @endforeach
-        </ul>
-        <div class="mt-4 text-lg font-semibold text-right">
-            Total: ${{ number_format($overallTotal, 2) }}
-        </div>
-        @endif
-</div>
+@if (session('cart') && count(session('cart')) > 0)
+    <div class="overflow-x-auto mt-6">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="py-3 px-6">Item</th>
+                    <th scope="col" class="py-3 px-6">Price</th>
+                    <th scope="col" class="py-3 px-6">Quantity</th>
+                    <th scope="col" class="py-3 px-6 text-end">Subtotal</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $overallTotal = 0; @endphp
+                @foreach (session('cart') as $id => $details)
+                    @php $overallTotal += $details['subtotal']; @endphp
+                    <x-cart.item :id="$id" :details="$details" />
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="mt-4 text-lg font-semibold text-right">
+        Total: ${{ number_format($overallTotal, 2) }}
+    </div>
+    <a href="{{ route('cart.checkout') }}"
+        class="mt-4 inline-block text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-sm px-6 py-2.5">Proceed
+        to Checkout</a>
+@else
+    <p class="mt-6 text-gray-700">Your cart is empty.</p>
+@endif
